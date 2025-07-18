720f11af560d3fee977d0b4e14b95465687b86e6
miguelsoffarelli <miguelsoffarelli11@gmail.com>
2025-07-17 23:36:14 -0300

    handlers and endpoints

--- main.go
+++ main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-	serveMux := http.NewServeMux()
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: serveMux,
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
 
-	handler := http.FileServer(http.Dir("."))
-	serveMux.Handle("/", handler)
-	log.Fatal(server.ListenAndServe())
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	body := fmt.Sprintf(`
+	<html>
+	  <body>
+	    <h1>Welcome, Chirpy Admin</h1>
+	    <p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>
+	`, cfg.fileserverHits.Load())
+	w.Write([]byte(fmt.Sprintf(body, cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
