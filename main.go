a710a14e0a69413f80e9349bf16fb0e6bd6a93f4
miguelsoffarelli <miguelsoffarelli11@gmail.com>
2025-07-18 21:37:19 -0300

    validate update and refactor

--- main.go
+++ main.go
@@ -20,8 +20,10 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidate)
+
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
--- validate.go
+++ validate.go
@@ -2,32 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
 func handlerValidate(w http.ResponseWriter, r *http.Request) {
-	// Two different types: one for OK response and the other for errors
-	type returnValsOK struct {
-		Valid bool `json:"valid"`
-	}
-
-	type returnValsErr struct {
-		Error string `json:"error"`
-	}
-
-	// this one is defined in the scope of the whole function because it'll be used more than once
-	errBody := returnValsErr{
-		Error: "Something went wrong",
-	}
-
-	errDat, err := json.Marshal(errBody)
-	if err != nil {
-		log.Printf("Error marshaling JSON: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
 	// decode request body
 	type parameters struct {
 		Body string `json:"body"`
@@ -35,44 +13,27 @@ func handlerValidate(w http.ResponseWriter, r *http.Request) {
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
-	err = decoder.Decode(&params)
+	err := decoder.Decode(&params)
 	if err != nil {
-		writeResponse(http.StatusBadRequest, errDat, w)
+		respondWithError(w, http.StatusBadRequest, "Something went wrong", err)
 		return
 	}
 
 	// length check
-	if len(params.Body) > 140 {
-		tooLongBody := returnValsErr{
-			Error: "Chirp is too long",
-		}
-
-		tooLongDat, err := json.Marshal(tooLongBody)
-		if err != nil {
-			writeResponse(http.StatusInternalServerError, errDat, w)
-			return
-		}
-
-		writeResponse(http.StatusBadRequest, tooLongDat, w)
+	const maxChiprLen = 140
+	if len(params.Body) > maxChiprLen {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 
 	// OK response
-	respBody := returnValsOK{
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	respBody := returnVals{
 		Valid: true,
 	}
 
-	OKDat, err := json.Marshal(respBody)
-	if err != nil {
-		writeResponse(http.StatusInternalServerError, errDat, w)
-		return
-	}
-
-	writeResponse(http.StatusOK, OKDat, w)
-}
-
-func writeResponse(code int, data []byte, w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(data)
+	respondWithJSON(w, http.StatusOK, respBody)
 }
