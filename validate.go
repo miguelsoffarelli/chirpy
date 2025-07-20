af785910e334a07619fffb51f786ccb92e455bae
miguelsoffarelli <miguelsoffarelli11@gmail.com>
2025-07-19 22:04:54 -0300

    clear chirp from profane words and database setup

--- validate.go
+++ validate.go
@@ -3,14 +3,19 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+type parameters struct {
+	Body string `json:"body"`
+}
+
+type cleanParams struct {
+	CleanedBody string `json:"cleaned_body"`
+}
 
 func handlerValidate(w http.ResponseWriter, r *http.Request) {
 	// decode request body
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
@@ -20,20 +25,25 @@ func handlerValidate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// length check
-	const maxChiprLen = 140
-	if len(params.Body) > maxChiprLen {
+	const maxChirpLen = 140
+	if len(params.Body) > maxChirpLen {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 
 	// OK response
-	type returnVals struct {
-		Valid bool `json:"valid"`
+	respondWithJSON(w, http.StatusOK, clearChirp(params))
+}
+
+func clearChirp(p parameters) cleanParams {
+	body := strings.Split(p.Body, " ")
+	for i, word := range body {
+		if strings.ToLower(word) == "kerfuffle" || strings.ToLower(word) == "sharbert" || strings.ToLower(word) == "fornax" {
+			body[i] = "****"
+		}
 	}
 
-	respBody := returnVals{
-		Valid: true,
+	return cleanParams{
+		CleanedBody: strings.Join(body, " "),
 	}
-
-	respondWithJSON(w, http.StatusOK, respBody)
 }
